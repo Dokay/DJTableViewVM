772446bfccc04515fbee5d649489e4783708ac89
Dokay <dokay.dou@gmail.com>
2018-01-18 15:52:34 +0800

    fix: reset constraints not work

--- DJTableViewVM/Classes/AdvanceCells/DJTableViewVMLinesTextCell.m
+++ DJTableViewVM/Classes/AdvanceCells/DJTableViewVMLinesTextCell.m
@@ -11,6 +11,8 @@
 @interface DJTableViewVMLinesTextCell()
 
 @property (nonatomic, strong) UILabel *multipleLinesLabel;
+@property (nonatomic, strong) NSArray *labelVerticalConstraints;
+@property (nonatomic, strong) NSArray *labelHorizontalConstraints;
 
 @end
 
@@ -36,22 +38,33 @@
     self.multipleLinesLabel.textColor = self.rowVM.titleColor;
     self.multipleLinesLabel.textAlignment = self.rowVM.titleTextAlignment;
     self.multipleLinesLabel.lineBreakMode = self.rowVM.lineBreakMode;
+    if (self.rowVM.titleAttributedString) {
+        self.multipleLinesLabel.attributedText = self.rowVM.titleAttributedString;
+    }
     
     [self updateLabelLayout];
 }
 
 - (void)updateLabelLayout
 {
-    [self.multipleLinesLabel.constraints enumerateObjectsUsingBlock:^(__kindof NSLayoutConstraint * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        [self.contentView removeConstraint:obj];
-    }];
+    if (self.multipleLinesLabel.constraints.count > 0) {
+        [self.multipleLinesLabel removeConstraints:self.multipleLinesLabel.constraints];
+    }
+    if (self.labelVerticalConstraints.count > 0) {
+        [self.contentView removeConstraints:self.labelVerticalConstraints];
+    }
+    if (self.labelHorizontalConstraints.count > 0) {
+        [self.contentView removeConstraints:self.labelHorizontalConstraints];
+    }
     
     NSDictionary *metrics=@{@"leftMargin":@(self.rowVM.elementEdge.left),
                             @"topMargin":@(self.rowVM.elementEdge.top),
                             @"rightMargin":@(self.rowVM.elementEdge.right),
                             @"bottomMargin":@(self.rowVM.elementEdge.bottom),};
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-topMargin-[_multipleLinesLabel]-bottomMargin-|" options:0 metrics:metrics views:NSDictionaryOfVariableBindings(_multipleLinesLabel)]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-leftMargin-[_multipleLinesLabel]-rightMargin-|" options:0 metrics:metrics views:NSDictionaryOfVariableBindings(_multipleLinesLabel)]];
+    self.labelVerticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-topMargin-[_multipleLinesLabel]-bottomMargin-|" options:0 metrics:metrics views:NSDictionaryOfVariableBindings(_multipleLinesLabel)];
+    [self.contentView addConstraints:self.labelVerticalConstraints];
+    self.labelHorizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-leftMargin-[_multipleLinesLabel]-rightMargin-|" options:0 metrics:metrics views:NSDictionaryOfVariableBindings(_multipleLinesLabel)];
+    [self.contentView addConstraints:self.labelHorizontalConstraints];
 }
 
 #pragma mark - getter
